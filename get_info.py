b87d90299a3b2bf65166f9891f4ada6ce95a66df
ethanc-ec <44012676+ethanc-ec@users.noreply.github.com>
2022-09-27 14:53:02 -0400

    temporary organizer/cleaner for section_finder() in help_funcs.py

--- get_info.py
+++ get_info.py
@@ -20,6 +20,8 @@ def content_getter(finder: str, input_class: str, yearsem: str = 'future') -> Be
         code_joined = ''.join(code).lower()
         URL = f"https://www.bu.edu/phpbin/course-search/section/?t={code_joined}&semester={time}&return=%2Fphpbin%2Fcourse-search%2Fsearch.php%3Fpage%3Dw0%26pagesize%3D10%26adv%3D1%26nolog%3D%26search_adv_all%3D{code[0]}%2B{code[1]}%2B{code[2]}%26yearsem_adv%3D{time}%26credits%3D%2A%26pathway%3D%26hub_match%3Dall"
     
+    print(URL)
+    
     page = requests.get(URL)
     content = BeautifulSoup(page.content, 'html.parser')
     
@@ -49,7 +51,9 @@ def info_finder(input_class: str, yearsem: str = 'future') -> dict:
         'hub credit' : hub_list[:]
     }
     
-    return cleaner(full_dict)
+    cleaned = cleaner(full_dict)
+    
+    return cleaned
     
 
 def section_finder(input_class: str, yearsem: str = 'future') -> dict:
@@ -58,16 +62,19 @@ def section_finder(input_class: str, yearsem: str = 'future') -> dict:
     for idx, val in enumerate(results):
         results[idx] = re.sub('<[^>]+>', '', str(val))[1:]
     
-    return results
+    organized = organize_class(results)
+    
+    return organized
     
     
 
 if __name__ == '__main__':
-    class_code = 'cdsds121'
-    yearsem = '2023 Sprg'
-    hub = info_finder(class_code, yearsem)
-    print_hub = True
+    class_code = 'cdsds100'
+    yearsem = '2022 fall'
     
+    print_hub, print_sections = True, True
+    
+    hub = info_finder(class_code, yearsem)
     if print_hub:
         for i in hub:
             if i != 'description':
@@ -76,4 +83,7 @@ if __name__ == '__main__':
                 print(f'{i}: {hub[i]} \n')
     
     sections = section_finder(class_code, yearsem)
-    print(sections)
+    if print_sections:
+        print('All sections:')
+        for i in sections:
+            print(f'{i}\n')
